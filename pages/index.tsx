f78fe6fc9c1e894ddca9fdcee4273df3f107a8ad
btahir89 <bilal.tahir89@gmail.com>
2022-02-03 19:16:21 -0800

    setup main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,68 +1,39 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Your Wordle</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
+      <main className="flex w-full flex-1 flex-col items-center justify-start px-20 text-center">
+        <h1 className="mt-6 text-3xl font-bold sm:text-4xl">
           Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+          <a className="text-blue-600 inline-block" href="https://nextjs.org">
+            Your Wordle
           </a>
         </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
+        <p className="mt-3 text-base sm:text;lg text-slate-600">
+          Generate your custom wordle and share with friends.
         </p>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="mt-6 space-y-12">
+          <div className="mb-12 flex items-end">
+            <div className="mr-2 text-lg font-semibold">Your Word</div>
+            <input
+              className="w-64 rounded-lg border border-blue-200 bg-blue-50 px-2 py-1 placeholder-blue-400"
+              placeholder="Add your word"
+            />
+          </div>
+          <Link passHref href="/">
+            <a className="rounded-lg bg-blue-600 px-3 py-2 font-medium text-white hover:bg-blue-700">
+              Generate Wordle
+            </a>
+          </Link>
         </div>
       </main>
 
